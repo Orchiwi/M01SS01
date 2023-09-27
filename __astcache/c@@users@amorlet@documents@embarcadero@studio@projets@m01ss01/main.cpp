e8c65b698fea51e2f3ba087e6e8ac6a2009349b6
Allan Morlet <allan.morlet13@gmail.com>
2023-09-27 17:02:31 +0200

    version avec message à la fin du programme

--- __astcache/c@@users@amorlet@documents@embarcadero@studio@projets@m01ss01/main.cpp
+++ __astcache/c@@users@amorlet@documents@embarcadero@studio@projets@m01ss01/main.cpp
@@ -2,17 +2,37 @@
 
 int main()
 {
-	float val1,val2,résultat;
+	float val1,val2,resultat;
 	char operateur;
 
 	std::cout << "Saisir calcul : ";
 	std::cin >> val1 >> operateur >> val2;
-	if (operateur == "+";) {
 
-	résultat=val1 + val2;
+	if (operateur == '+')
+	{
 
-	std::cout << "Somme : "<< résultat << std::endl;
+		resultat=val1 + val2;
+
+		std::cout << "Somme : "<< resultat << std::endl;
+	}
+	else if(operateur == '-'){
+		resultat=val1 - val2;
+		std::cout << "Soustraction : "<< resultat << std::endl;
+	}
+	else if(operateur == '*'){
+		resultat=val1 * val2;
+		std::cout << "Multiplication : "<< resultat << std::endl;
+	}
+		else if(operateur == '/'){
+		resultat=val1 / val2;
+		std::cout << "Division : "<< resultat << std::endl;
+	}
+		else {std::cout << "Erreur" << std::endl;
+		system("PAUSE");
+		return -1;}
+
+
 	system("PAUSE");
 
 	return 0;
-}}
+}
