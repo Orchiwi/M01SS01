c33e64d4c470f705f0afd890591209579396d34a
MORLET Allan <AMORLET>
2023-09-28 09:25:55 +0200

    Gestion du fichier log

--- __astcache/c@@users@amorlet@documents@embarcadero@studio@projets@m01ss01/main.cpp
+++ __astcache/c@@users@amorlet@documents@embarcadero@studio@projets@m01ss01/main.cpp
@@ -1,37 +1,45 @@
 ﻿#include <iostream>
-
+#include <fstream>
+using namespace std;
 int main()
 {
 	float val1,val2,resultat;
 	char operateur;
 
-	std::cout << "Saisir calcul : ";
-	std::cin >> val1 >> operateur >> val2;
+	cout << "Saisir calcul : ";
+	cin >> val1 >> operateur >> val2;
 
 	if (operateur == '+')
 	{
 
 		resultat=val1 + val2;
 
-		std::cout << "Somme : "<< resultat << std::endl;
+		cout << "Somme : "<< resultat << endl;
 	}
 	else if(operateur == '-'){
 		resultat=val1 - val2;
-		std::cout << "Soustraction : "<< resultat << std::endl;
+		cout << "Soustraction : "<< resultat << endl;
 	}
 	else if(operateur == '*'){
 		resultat=val1 * val2;
-		std::cout << "Multiplication : "<< resultat << std::endl;
+		cout << "Multiplication : "<< resultat << endl;
 	}
 		else if(operateur == '/'){
 		resultat=val1 / val2;
-		std::cout << "Division : "<< resultat << std::endl;
+		cout << "Division : "<< resultat << endl;
 	}
-		else {std::cout << "Erreur" << std::endl;
+		else {cout << "Erreur" << endl;
 		system("PAUSE");
-		return -1;}
+		return -1;
+	}
 
-    std::cout << "FIN DU PROGRAMME CALCULATRICE SANS ERREUR" << std::endl
+	cout << "FIN DU PROGRAMME CALCULATRICE SANS ERREUR" << endl;
+
+	ofstream FichierLog;
+	FichierLog.open("journal log", ios_base::app);
+	FichierLog << val1 << operateur << val2 << ":" << resultat << "\n";
+	FichierLog.close ();
+
 	system("PAUSE");
 
 	return 0;
